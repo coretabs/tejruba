e08775d7a5eca4a54cbbd5394c9b96c767e82dd3
0xThamer <thamertx0@gmail.com>
2018-07-16 21:22:26 +0300

    add models > Experiment, Useful, NotUseful

--- tejruba/experiment/models.py
+++ tejruba/experiment/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Experiment(models.Model):
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='experiments')
+
+    def __str__(self):
+        return self.content[:30]
+
+
+class Useful(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='usefuls')
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE, related_name='usefuls')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user}, Useful {self.experiment}'
+
+
+class NotUseful(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notusefuls')
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE, related_name='notusefuls')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        
+        return f'{self.user}, NotUseful {self.experiment}'
+
