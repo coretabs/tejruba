a1884112d49c344e067b7a6683587f5df24a665e
makhloufi <makhloufi200@gmail.com>
2018-07-17 13:38:54 +0100

    add experiment template

--- tejruba/experiment/views.py
+++ tejruba/experiment/views.py
@@ -3,16 +3,16 @@ from .models import Experiment, Useful, NotUseful
 from django.http import Http404
 # Create your views here.
 
-
+#list of experiments
 def experiments_list(request, slug=None):
     experiments = Experiment.objects.all()
     return render(request, 'list_experiments.html', {'experiments': experiments})
 
 
-
+# detail of experiment
 def experiment_detail_view(request,pk):
         try:
-            experiment_id=Experiment.objects.get(pk=pk)
+            experiment_id=Experiment.objects.get(pk=pk)# get id of experiment
         except Experiment.DoesNotExist:
             raise Http404("Experiment does not exist")
 
