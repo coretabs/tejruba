be1164d0882d8658b94104468be24c9d203735a6
Thamer <thamertx0@gmail.com>
2018-07-20 21:04:17 +0300

    add: Experiment update

--- tejruba/experiment/urls.py
+++ tejruba/experiment/urls.py
@@ -8,5 +8,6 @@ experiments/delete/<int:pk>
 urlpatterns = [
     path('', views.experiments_list, name='experiments_list'),
     path('experiments/<int:pk>', views.experiment_detail_view, name='experiment-detail'),
-    path('experiments/add', views.create_experiment, name="create_experiment")
+    path('experiments/add', views.create_experiment, name="create_experiment"),
+    path('experiments/<int:pk>/edit', views.experiment_update, name="experiment_update")
 ]
--- tejruba/experiment/views.py
+++ tejruba/experiment/views.py
@@ -1,27 +1,19 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
 from .models import Experiment
 from .forms import ExperimentForm
 
 
 #list of experiments
-def experiments_list(request, slug=None):
+def experiments_list(request):
     experiments = Experiment.objects.all()
     return render(request, 'list_experiments.html', {'experiments': experiments})
 
 
 # detail of experiment
 def experiment_detail_view(request, pk):
-        try:
-            experiment_id = Experiment.objects.get(pk=pk)  # get id of experiment
-        except Experiment.DoesNotExist:
-            raise Http404("Experiment does not exist")
-
-        return render(
-            request,
-            'experiment_detail.html',
-            context={'experiment': experiment_id, }
-        )
+    experiment_detail = get_object_or_404(Experiment, pk=pk)
+    return render(request, 'experiment_detail.html', context={'experiment': experiment_detail})
 
 
 def create_experiment(request):
@@ -35,5 +27,24 @@ def create_experiment(request):
         instance.save()
         return redirect('experiments_list')
 
-    return render(request, 'create_experiment.html', {'form': form})
+    return render(request, 'experiment_form.html', {'form': form})
 
+
+def experiment_update(request, pk):
+    if not request.user.is_authenticated:
+        raise Http404
+
+    instance = get_object_or_404(Experiment, pk=pk)
+
+    if instance.author != request.user:
+        raise Http404
+
+    form = ExperimentForm(request.POST or None, instance=instance)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return redirect(instance)
+
+    return render(request, 'experiment_form.html', {"form": form,
+                                                    "instance": instance})
