d3efe26c72adcaa2dfca10c4872310db9fde117b
makhloufi <makhloufi200@gmail.com>
2018-10-02 08:49:21 +0100

    add serializers

--- tejruba/experiment/serializers.py
+++ tejruba/experiment/serializers.py
@@ -10,3 +10,20 @@ class ExperimentSerializer(serializers.ModelSerializer):
                   "content",
                   "created",
         )
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Experiment` instance, given the validated data.
+        """
+        return Experiment.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Experiment` instance, given the validated data.
+        """
+        instance.author = validated_data.get('author', instance.author)
+        instance.content = validated_data.get('content', instance.content)
+        instance.created = validated_data.get('created', instance.created)
+
+        instance.save()
+        return instance
--- tejruba/experiment/urls.py
+++ tejruba/experiment/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.experiments_list, name='experiments_list'),
     path('experiments/<int:pk>/', views.experiment_detail_view, name='experiment-detail'),
    
-    path('experiments/add/', views.create_experiment, name="create_experiment"),
+    path('post', views.experiment_list, name="create_experiment"),
     path('experiments/<int:pk>/edit/', views.experiment_update, name="experiment_update"),
     path('experiments/<int:pk>/delete/', views.experiment_delete, name="experiment_delete"),
    
