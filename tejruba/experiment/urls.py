8bfc623a06cd0726108578c107d9bf69dcdc5a78
makhloufi <makhloufi200@gmail.com>
2018-10-21 13:57:46 +0100

    add exeperiment update and delete

--- tejruba/experiment/urls.py
+++ tejruba/experiment/urls.py
@@ -6,22 +6,23 @@ experiments/<int:pk>/edit
 experiments/<int:pk>/delete
 '''
 urlpatterns = [
-    path('', views.experiments_list, name='experiments_list'),
-    path('experiments/<int:pk>/', views.experiment_detail_view, name='experiment-detail'),
-   
-    path('experiments/add/', views.create_experiment, name="create_experiment"),
-    path('experiments/<int:pk>/edit/', views.experiment_update, name="experiment_update"),
-    path('experiments/<int:pk>/delete/', views.experiment_delete, name="experiment_delete"),
-   
-    path('experiments/<int:pk>/useful/', views.ExperimentUsefulRedirect.as_view(), name='experiment_useful'),
-    path('experiments/<int:pk>/notuseful/', views.ExperimentNotUsefulRedirect.as_view(), name='experiment_notuseful'),
-   
-    path('api/experiments/<int:pk>/useful/', views.ExperimentUsefulAPI.as_view(), name='experiment_api_useful'),
-    path('api/experiments/<int:pk>/notuseful/', views.ExperimentNotUsefulAPI.as_view(), name='experiment_api_notuseful'),
-   
-    path('user/', views.update_profile, name="profile"),
-    path('user/<slug>/', views.profile_view, name="profile_view"),
-    path('signup/', views.signup, name='signup'),
-    #path('experiment', views.ExperimentList, name="create_experiment"),
+    #path('', views.experiments_list, name='experiments_list'),
+    #path('experiments/<int:pk>/', views.experiment_detail_view, name='experiment-detail'),
+    #
+    #path('experiments/add/', views.create_experiment, name="create_experiment"),
+    #path('experiments/<int:pk>/edit/', views.experiment_update, name="experiment_update"),
+    #path('experiments/<int:pk>/delete/', views.experiment_delete, name="experiment_delete"),
+    #
+    #path('experiments/<int:pk>/useful/', views.ExperimentUsefulRedirect.as_view(), name='experiment_useful'),
+    #path('experiments/<int:pk>/notuseful/', views.ExperimentNotUsefulRedirect.as_view(), name='experiment_notuseful'),
+    #
+    #path('api/experiments/<int:pk>/useful/', views.ExperimentUsefulAPI.as_view(), name='experiment_api_useful'),
+    #path('api/experiments/<int:pk>/notuseful/', views.ExperimentNotUsefulAPI.as_view(), name='experiment_api_notuseful'),
+    #
+    #path('user/', views.update_profile, name="profile"),
+    #path('user/<slug>/', views.profile_view, name="profile_view"),
+    #path('signup/', views.signup, name='signup'),
+    path('experiments/', views.Experiments_list),
+    path('experiments/<int:pk>', views.Experiments_detail),
 
 ]
--- tejruba/experiment/views.py
+++ tejruba/experiment/views.py
@@ -9,220 +9,52 @@ from django.contrib.auth import login, authenticate
 from django.views.generic import RedirectView
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, generics
 from .serializers import ExperimentSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-#list of experiments
-def experiments_list(request):
-    experiments = Experiment.objects.all()
-    return render(request, 'list_experiments.html', {'experiments': experiments})
-
-
-# detail of experiment
-def experiment_detail_view(request, pk):
-    experiment_detail = get_object_or_404(Experiment, pk=pk)
-    return render(request, 'experiment_detail.html', context={'experiment': experiment_detail})
-
-
-# Useful
-class ExperimentUsefulRedirect(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        pk = self.kwargs.get("pk")
-        obj = get_object_or_404(Experiment, pk=pk)
-        url_ = obj.get_absolute_url()
-        user = self.request.user
-        if user.is_authenticated:
-            if obj.usefuls.filter(pk=user.pk):
-                obj.usefuls.remove(user)
-            else:
-                if obj.notusefuls.filter(pk=user.pk):
-                    obj.notusefuls.remove(user)
-                obj.usefuls.add(user)
-        return url_
-
-
-# Useful API
-class ExperimentUsefulAPI(APIView):
-    authentication_classes = (authentication.SessionAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get(self, request, pk, format=None):
-        obj = get_object_or_404(Experiment, pk=pk)
-        user = self.request.user
-        updated = False
-        useful = False
-
-        if user.is_authenticated:
-            if obj.usefuls.filter(pk=user.pk):
-                obj.usefuls.remove(user)
-                useful = False
-            else:
-                if obj.notusefuls.filter(pk=user.pk):
-                    obj.notusefuls.remove(user)
-                useful = True
-                obj.usefuls.add(user)
-            updated = True
-        data = {
-            "updated": updated,
-            "useful": useful
-        }
-
-        return Response(data)
-
-
-# Not Useful
-class ExperimentNotUsefulRedirect(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        pk = self.kwargs.get("pk")
-        obj = get_object_or_404(Experiment, pk=pk)
-        url_ = obj.get_absolute_url()
-        user = self.request.user
-        if user.is_authenticated:
-            if obj.notusefuls.filter(pk=user.pk):
-                obj.notusefuls.remove(user)
-            else:
-                if obj.usefuls.filter(pk=user.pk):
-                    obj.usefuls.remove(user)
-                obj.notusefuls.add(user)
-        return url_
-
-
-# Not Useful API
-class ExperimentNotUsefulAPI(APIView):
-    authentication_classes = (authentication.SessionAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get(self, request, pk, format=None):
-        obj = get_object_or_404(Experiment, pk=pk)
-        user = self.request.user
-        updated = False
-        notuseful = False
-
-        if user.is_authenticated:
-            if obj.notusefuls.filter(pk=user.pk):
-                obj.notusefuls.remove(user)
-                notuseful = False
-            else:
-                if obj.usefuls.filter(pk=user.pk):
-                    obj.notusefuls.remove(user)
-                notuseful = True
-                obj.notusefuls.add(user)
-            updated = True
-        data = {
-            "updated": updated,
-            "notuseful": notuseful
-        }
-        return Response(data)
-
-
-def create_experiment(request):
-    if not request.user.is_authenticated:
-        raise Http404
-
-    form = ExperimentForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.author = request.user
-        instance.save()
-        return redirect('experiments_list')
-
-    return render(request, 'experiment_form.html', {'form': form})
-
-
-def experiment_update(request, pk):
-    if not request.user.is_authenticated:
-        raise Http404
-
-    instance = get_object_or_404(Experiment, pk=pk)
-
-    if instance.author != request.user:
-        raise Http404
-
-    form = ExperimentForm(request.POST or None, instance=instance)
-
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-        return redirect(instance)
-
-    return render(request, 'experiment_form.html', {"form": form,
-                                                    "instance": instance})
-
-
-def experiment_delete(request, pk):
-    instance = get_object_or_404(Experiment, pk=pk)
-
-    if request.user == instance.author:
-        instance.delete()
-
-    return redirect("experiments_list")
-
-
-@login_required
-@transaction.atomic
-def update_profile(request):
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, ('Your profile was successfully updated!'))
-            return redirect('experiments_list')
-        else:
-            messages.error(request, ('Please correct the error below.'))
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'profile.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
-
-def profile_view(request, slug):
-    profile = get_object_or_404(Profile, slug=slug)
-    return render(request, 'profile_view.html', {'profile': profile})
-
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect('experiments_list')
-    else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
-
-
-class ExperimentList(viewsets.ModelViewSet):
+from rest_framework.decorators import api_view
+   #model = Experiment
+@api_view(['GET', 'POST'])
+def Experiments_list(request):
     """
-    List all experiments, or create a new experiments.
+    List all code experiments, or create a new experiment
     """
-    #def get(self, request, format=None):
-    #    experiments = Experiment.objects.all()
-    #    serializer = ExperimentSerializer(experiments, many=True)
-    #    return Response(serializer.data)
-    #
-    #def post(self, request, format=None):
-    #    serializer = ExperimentSerializer(data=request.data)
-    #    if serializer.is_valid():
-    #        serializer.save()
-    #        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        experiments = Experiment.objects.all()
+        serializer = ExperimentSerializer(experiments, many=True)
+        return Response(serializer.data)
 
-
-    def create(self, request):
+    elif request.method == 'POST':
         serializer = ExperimentSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(author=self.request.user)
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def Experiments_detail(request, pk):
+    """
+    Retrieve, update or delete experiment
+    """
+    try:
+        experiment = Experiment.objects.get(pk=pk)
+    except Experiment.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ExperimentSerializer(experiment)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ExperimentSerializer(experiment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        experiment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
